7b93dab178abd5a9f767546558282a4458d9899c
root2 <137818850_2@qq.com>
2019-01-12 23:05:21 +0800

    添加了user类

---
+++ src/com/fly/bean/User.java
@@ -0,0 +1,61 @@
+package com.fly.bean;
+
+import java.io.Serializable;
+
+/**
+ * @author liang
+ * @date 2019/1/12 - 23:02
+ */
+public class User implements Serializable{
+
+    private Integer id;
+    private String username;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public User(Integer id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+}
